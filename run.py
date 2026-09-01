ae32f87d57b441c804f630724f54f3ee4f2b7e25
Lorenzo <lorenzo@lrzodev.xyz>
2026-09-01 02:38:02 +0100

    Update CI to utilise run.py

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from src.matrix_forge.app import main 
+import sys
+import os
+
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "src"))
+sys.path.insert(0, src_path)
+
+from matrix_forge.app import main
 
 if __name__ == "__main__":
     main()
